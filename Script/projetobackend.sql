5bfb0f21526e469f361d85832da7af3c3ee1e1dc
Unknown <luiz.c.brito@Hotmail.com>
2018-03-11 22:43:07 -0300

    pequenas mudanças

--- Script/projetobackend.sql
+++ Script/projetobackend.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 12-Mar-2018 às 00:06
+-- Generation Time: 12-Mar-2018 às 02:42
 -- Versão do servidor: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -33,36 +33,9 @@ CREATE TABLE `usuario` (
   `nome` varchar(128) NOT NULL,
   `idade` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `senha` varchar(100) NOT NULL,
   `foto` varchar(512) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `usuario`
---
-
-INSERT INTO `usuario` (`id`, `nome`, `idade`, `email`, `foto`) VALUES
-(32, 'qwe', 0, 'qwe', '-Dragon Ball Super - EP. 121 [720p] [LEG] COMANDOTORRENTS.COM.mp4_snapshot_00.02.jpg'),
-(33, 'qwe123', 22, 'qwe123', 'c3991b714340fa291c3343b5ee0ba44907bcfcd7-Dragon Ball Super - EP. 121 [720p] [LEG] COMANDOTORRENTS.COM.mp4_snapshot_00.02.jpg'),
-(34, 'qwe', 0, 'qwe', 'c3991b714340fa291c3343b5ee0ba44907bcfcd7-Dragon Ball Super - EP. 121 [720p] [LEG] COMANDOTORRENTS.COM.mp4_snapshot_00.02.jpg'),
-(35, 'ads', 22, 'dasd', ''),
-(36, 'ads', 0, 'dasd', ''),
-(37, 'ads', 0, 'dasd', ''),
-(38, 'qwe', 0, 'LUIZ.C.BRIT2O@HOTMAIL.COM', ''),
-(39, 'asd', 22, 'teste@teste.com', ''),
-(40, 'qwe', 0, 'qwe', ''),
-(41, 'luizcarlos', 20, 'luiz@luiz.com', ''),
-(42, 'czxcbv', 0, 'vxv', ''),
-(43, 'dasdvx', 0, 'bvnvbn', ''),
-(44, 'gdfhhfgh', 0, 'hfgh', ''),
-(45, 'hfghfgh', 0, 'nvbnvb', ''),
-(46, 'hgfhsdf', 0, 'sfsdf', ''),
-(47, 'dfgdfgc', 0, 'ghfh', ''),
-(48, 'TESTE', 22, 'LUIZ.C.BRIT2O@HOTMAIL.COM', 'c3991b714340fa291c3343b5ee0ba44907bcfcd7-dbz.jpg'),
-(49, 'qwe', 0, 'zxc', ''),
-(50, 'Luiz', 23, 'luiz.c.brito1@gmail.com', 'c3991b714340fa291c3343b5ee0ba44907bcfcd7-dbz.jpg'),
-(51, 'Luiz', 23, 'luiz.c.brito1@gmail.com', 'c3991b714340fa291c3343b5ee0ba44907bcfcd7-dbz.jpg'),
-(52, 'Luiz', 23, 'luiz.c.brito1@gmail.com', '-dbz.jpg'),
-(53, 'Luiz', 23, 'luiz.c.brito1@gmail.com', 'c3991b714340fa291c3343b5ee0ba44907bcfcd7-dbz.jpg');
 
 --
 -- Indexes for dumped tables
@@ -82,7 +55,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
